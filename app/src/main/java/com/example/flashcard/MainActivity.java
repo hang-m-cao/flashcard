5bc4124fd516379eaf388f52664cec396d8020a9
Hang Cao <hmcao1@uci.edu>
2021-02-20 11:09:42 -0800

    first question and answer toggle

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.flashcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView question = findViewById(R.id.flashcard_question);
+        TextView answer = findViewById(R.id.flashcard_answer);
+
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                question.setVisibility(View.INVISIBLE);
+                answer.setVisibility(View.VISIBLE);
+            }
+        });
+
+        answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer.setVisibility(View.INVISIBLE);
+                question.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
